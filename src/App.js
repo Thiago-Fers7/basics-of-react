fef2631b7575030c46ce7b74fa5fddc34575a59c
Luiz Thiago <thiago_fers7@hotmail.com>
2021-09-26 17:57:15 -0300

    useRef

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { ThemeProvider } from 'styled-components'
 
 import GlobalStyle from './styles/global';
@@ -9,11 +9,20 @@ import themes from './styles/themes'
 function App() {
   const [theme, setTheme] = useState(JSON.parse(localStorage.getItem('theme')) || 'light');
 
+  const firstRender = useRef(true) // useRef para manter o valor durante os renders
+
   const currentTheme = useMemo(() => {
     return themes[theme] || themes.dark
   }, [theme]);
 
   useEffect(() => {
+    if (firstRender.current) {
+      firstRender.current = false
+      return
+    }
+
+    console.log('A partir do segundo render')
+
     localStorage.setItem('theme', JSON.stringify(theme))
   }, [theme])
 
